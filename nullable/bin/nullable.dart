611caa295d768be9cd314fc51ae79af2ffbb7105
rastaanra <rastaaci82@gmail.com>
2026-03-03 23:25:33 +0700

    praktikum mobile

---
+++ nullable/bin/nullable.dart
@@ -0,0 +1,8 @@
+import 'package:nullable/nullable.dart' as nullable;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${nullable.calculate()}!');
+  String name;
+  name = null;
+  print(name);
+}
